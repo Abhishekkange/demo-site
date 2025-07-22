0aebc54dcb95e44d2daa4c40ff0e79ad5086e09e
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2025-07-22 22:25:22 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+// server.js
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/submit', (req, res) => {
+  const formData = req.body;
+  console.log('Form Data:', formData);
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'abhishekkange@gmail.com',
+      pass: 'mole iapa xxyu gyfn'
+    }
+  });
+
+  const mailOptions = {
+    from: 'abhishekkange@gmail.com',
+    to: 'abhishekkange00@gmail.com',
+    subject: 'New Internship Application',
+    text: JSON.stringify(formData)
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) return res.status(500).send('Email failed');
+    return res.status(200).send('Email sent successfully');
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
